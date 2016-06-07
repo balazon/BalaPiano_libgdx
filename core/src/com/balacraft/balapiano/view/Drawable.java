f926b5cdc7a920418f86bc7abf3a115c7fd008ae
balazon <balazon@gmail.com>
2016-06-07 22:44:35 +0200

    cleaning up some code

--- core/src/com/balacraft/balapiano/view/Drawable.java
+++
@@ -1,8 +0,0 @@
-package com.balacraft.balapiano.view;
-
-import com.badlogic.gdx.graphics.Camera;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
-public interface Drawable {
-	public void draw(SpriteBatch batch, Camera cam);
-}
