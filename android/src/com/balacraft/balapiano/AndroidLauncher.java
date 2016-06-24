cc641e472a17a6de1377f2f733e6aa59b3069e27
balazon <balazon@gmail.com>
2016-06-24 17:46:13 +0200

    minor changes (icon, android immersive sticky mode)

--- android/src/com/balacraft/balapiano/AndroidLauncher.java
+++ android/src/com/balacraft/balapiano/AndroidLauncher.java
@@ -2,6 +2,7 @@ package com.balacraft.balapiano;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -28,6 +29,20 @@ public class AndroidLauncher extends AndroidApplication {
 
 
 	///////////////////////////////////////////////////////////////////////////
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+		super.onWindowFocusChanged(hasFocus);
+		if (hasFocus) {
+			getWindow().getDecorView().setSystemUiVisibility(
+					View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+					| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+					| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+					| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+					| View.SYSTEM_UI_FLAG_FULLSCREEN
+					| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+		}
+	}
+
 	@Override
 	protected void onStart() {
 		super.onStart();
